65c11ce576473a5d20767da93b8b6e09fb5755f8
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-08-09 19:12:32 +0900

    ver 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cbiprep',
-    version='0.1.2',
+    version='0.1.3',
     description='CBI Prep',
     author='Ryuichiro Hara',
     author_email='hara.ryuichiro@gmail.com',
