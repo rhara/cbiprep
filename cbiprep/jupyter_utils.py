6148e86736e488b4c849d2cfcc8c320ad128ab04
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2020-08-12 03:41:11 +0900

    ver 0.1.14

--- cbiprep/jupyter_utils.py
+++ cbiprep/jupyter_utils.py
@@ -2,6 +2,7 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.Chem.Draw import rdMolDraw2D
 from IPython.display import SVG
+import subprocess as sp
 
 
 def draw_mol(mol, w=300, h=150):
@@ -9,4 +10,8 @@ def draw_mol(mol, w=300, h=150):
     d.DrawMolecule(mol)
     d.FinishDrawing()
     display(SVG(d.GetDrawingText()))
+
+
+def check_dir(pdb_code, workdir='work'):
+    print(sp.check_output(f'ls -l {workdir}/{pdb_code}', shell=True, universal_newlines=True))
 
